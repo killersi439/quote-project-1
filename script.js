3cc6e575e9cb18ba8dc8fec2ad0fd15dd889892c
killersi439 <kevinmwaniki439@gmail.com>
2025-03-27 12:24:52 +0300

    first script

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log ("name");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
