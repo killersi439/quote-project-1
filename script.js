28894019e30dea79905e001f513ef2b31a857671
killersi439 <kevinmwaniki439@gmail.com>
2025-03-29 13:03:42 +0300

    js day 2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // giving the variables the value of the elements in the html file
-//const quoteContainer = document.getElementById("quote-container");
-//const quoteText = document.getElementById("quote");
-//const authorText = document.getElementById("author");
-//const twitterBtn = document.getElementById("twitter");
-//const newQuoteBtn = document.getElementById("new-quote");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
  //Get Quote From API
 //fetch("https://type.fit/api/quotes")
